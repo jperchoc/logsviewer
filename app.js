56b89102476c9e9d133677ff5f9dc267fa77c723
es265 <jonathan.perchoc@partnre.com>
2020-01-02 11:25:41 +0100

    Code cleanup - use vutify

--- app.js
+++ app.js
@@ -1,112 +1,135 @@
+///////////////////////////////////
+//            DATA               //
+///////////////////////////////////
+let chartOptions = {
+  theme: "light2",
+  animationEnabled: true,
+  zoomEnabled: true,
+  zoomType: "xy",
+  axisX: {
+    gridThickness: 1,
+    interval: 10,
+    intervalType: "minute",
+    valueFormatString: "HH:mm",
+    labelAngle: 45,
+    labelFontSize: 15,
+  },
+  axisY: {
+    title: "Temperature",
+    interlacedColor: "#F0F0F0",
+
+  },
+  legend: {
+    cursor: "pointer",
+    itemclick: function (e) {
+      if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+        e.dataSeries.visible = false;
+      } else {
+        e.dataSeries.visible = true;
+      }
+
+      e.chart.render();
+    }
+  },
+  toolTip: {
+    contentFormatter: function (e) {
+      var content = "";
+      for (var i = 0; i < e.entries.length; i++) {
+        content = e.entries[i].dataPoint.indexLabel ?
+          e.entries[i].dataPoint.indexLabel + ' @ ' + CanvasJS.formatDate(e.entries[i].dataPoint.x, "HH:mm:ss")
+          : e.entries[i].dataPoint.y + '°C @ ' + CanvasJS.formatDate(e.entries[i].dataPoint.x, "HH:mm:ss");
+      }
+      return content;
+    }
+  },
+  data: null
+}
+
+function readFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.readAsText(file, "UTF-8");
+    reader.onload = (evt) => {
+      resolve(evt.target.result);
+    };
+    reader.onerror = reject;
+  })
+}
+
+///////////////////////////////////
+//            APPLICATION        //
+///////////////////////////////////
+const vuetifyOptions = { }
+Vue.use(Vuetify)
 var app = new Vue({
   el: '#app',
+  vuetify: new Vuetify(vuetifyOptions),
   data: {
     chartRendered: false,
-    data: []
+    data: [],
+    files: [],
+    colors: [
+      { color: '#4661EE'},
+      { color: '#EC5657'},
+      { color: '#1BCDD1'},
+      { color: '#D1CBDC'},
+      { color: '#CBD1DC'}
+    ]
   },
   methods: {
     renderChart: async function () {
-
+      await this.processFiles(this.files);
       this.chartRendered = true;
-      console.log('data:', this.data)
-      var chart = new CanvasJS.Chart("chartContainer", {
-        theme: "light2", // "light1", "light2", "dark1", "dark2"
-        animationEnabled: true,
-        zoomEnabled: true,
-        zoomType: "xy",
-        axisX: {
-          gridThickness: 1,
-          interval: 15,
-          intervalType: "minute",
-          valueFormatString: "HH:mm",
-        },
-        axisY: {
-          title: "Temperature",
-          interlacedColor: "#F0F0F0",
-
-        },
-        legend: {
-          cursor: "pointer",
-          itemclick: function (e) {
-            if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-              e.dataSeries.visible = false;
-            } else {
-              e.dataSeries.visible = true;
-            }
-
-            e.chart.render();
-          }
-        },
-        toolTip: {
-          contentFormatter: function (e) {
-            var content = "";
-            for (var i = 0; i < e.entries.length; i++) {
-              content = e.entries[i].dataPoint.y + '°C @ ' + CanvasJS.formatDate(e.entries[i].dataPoint.x, "HH:mm:ss");
-            }
-            return content;
-          }
-        },
-        data: this.data
-      });
+      chartOptions.data = this.data;
+      var chart = new CanvasJS.Chart("chartContainer", chartOptions);
       chart.render();
     },
-    processFile: function () {
-      console.log('selected a file');
-      for (let i = 0; i < this.$refs.myFiles.files.length; i++) {
-        let file = this.$refs.myFiles.files[i];
-        this.prepareFile(file);
+    processFiles: async function (files) {
+      for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+        await this.prepareFile(file);
       }
     },
-    prepareFile(file) {
-      console.log('processing file', file)
-      let reader = new FileReader();
-      let params = null;
-      switch (file.name) {
-        case 'kettle_1.log': params = { name: 'kettle_1', color: '#4661EE', solid: false }; break;
-        case 'kettle_2.log': params = { name: 'kettle_1', color: '#EC5657', solid: false }; break;
-        case 'kettle_3.log': params = { name: 'kettle_1', color: '#1BCDD1', solid: false }; break;
-        case 'sensor_1.log': params = { name: 'sensor_1', color: '#4661EE', solid: true }; break;
-        case 'sensor_2.log': params = { name: 'sensor_2', color: '#EC5657', solid: true }; break;
-        case 'sensor_3.log': params = { name: 'sensor_3', color: '#1BCDD1', solid: true }; break;
-      }
-      if (params) {
-        reader.readAsText(file, "UTF-8");
-        reader.onload = evt => {
-          let text = evt.target.result;
-
-          //Process text
-          let data = text.split(/\r?\n/)
-          let formatedData = [];
-          for (let i = 0; i < data.length; i++) {
-            let rawData = data[i].split(',');
-            if (rawData.length === 2) {
-              const date = new Date(rawData[0])
-              const value = parseFloat(rawData[1])
-              formatedData.push({
-                x: date,
-                y: value < 0 ? 0 : value
-              });
-            }
+    async prepareFile(file) {
+      let text = await readFileAsync(file);
+      const isAction = file.name === 'action.log';
+      const lines = text.split(/\r?\n/)
+      let formatedData = [];
+      let color = 'black';
+      //Format Data
+      for (let i = 0; i < lines.length; i++) {
+        let rawData = lines[i].split(',');
+        let date = new Date(rawData[0]);
+        if (rawData.length === 2) {
+          let dataToAdd = { x: date };
+          //y value change if we are reading action.log file
+          if (isAction) {
+            dataToAdd.y = i % 2 === 0 ? 100 : 90;
+            dataToAdd.indexLabel = rawData[1].replace("Start Step", "");
+            dataToAdd.markerColor = 'darkred';
+            dataToAdd.markerType = "cross";
+          } else {
+            const value = parseFloat(rawData[1])
+            dataToAdd.y = value < 0 ? null : value;
+            color = this.colors[parseInt(file.name.split('_')[1]) - 1].color
           }
-          //add dataset
-          console.log('push data to array')
-          this.data.push({
-            type: 'line',
-            dataPoints: formatedData,
-            lineDashType: params.solid ? "solid" : "dash",
-            name: params.name,
-            lineColor: params.color,
-            color: params.color,
-            showInLegend: true,
-            legendText: params.name,
-            xValueType: "dateTime"
-          });
-        }
-        reader.onerror = evt => {
-          console.error(evt);
+          if (isAction) formatedData.push({ x: date, y: 0, });
+          formatedData.push(dataToAdd);
+          if (isAction) formatedData.push({ x: date, y: 0, });
         }
       }
-      console.log('end of file processing')
+      //Add data to chart
+      this.data.push({
+        type: 'line',
+        dataPoints: formatedData,
+        lineDashType: file.name.indexOf('sensor') >= 0 ? "solid" : "dash",
+        name: file.name.replace('.log', ''),
+        lineColor: color,
+        color: color,
+        showInLegend: !isAction,
+        legendText: file.name.replace('.log', ''),
+        xValueType: "dateTime"
+      });
     }
   }
 })
